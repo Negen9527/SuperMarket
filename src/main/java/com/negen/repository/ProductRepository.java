4b3d028f8aa33774d24b37623d89b50d68da99ff
negen <1096041251@qq.com>
2019-02-14 17:28:10 +0800

    下班20190214

---
+++ src/main/java/com/negen/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.negen.repository;
+
+import com.negen.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Long, Product> {
+
+}
