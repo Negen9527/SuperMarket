a6a8ada92080d001d597d6ce7d2f36b5533e29b4
negen <1096041251@qq.com>
2019-02-15 11:47:04 +0800

    [add]集成swagger2

--- src/main/java/com/negen/controller/ProductController.java
+++ src/main/java/com/negen/controller/ProductController.java
@@ -3,13 +3,17 @@ package com.negen.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.negen.entity.Product;
 import com.negen.repository.ProductRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Api("product相关API")
 @RestController
 @RequestMapping(value = "/product")
 public class ProductController {
@@ -21,6 +25,13 @@ public class ProductController {
      * @param jsonParam
      * @return
      */
+    @ApiOperation(value = "添加商品信息", notes = "")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "productName", value = "商品名称", required = true, dataType = "string"),
+            @ApiImplicitParam(name = "productBarCode", value = "商品条码", required = true, dataType = "string"),
+            @ApiImplicitParam(name = "productPrice", value = "商品价格", required = true, dataType = "double"),
+            @ApiImplicitParam(name = "productCount", value = "商品数量", required = true, dataType = "int")
+    })
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public JSONObject addProduct(@RequestBody JSONObject jsonParam){
         JSONObject resultJson = new JSONObject();
